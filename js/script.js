42b11431e2af8343adbd57397a6e3807e2cee2ee
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-05-26 20:24:18 +0700

    feat: disable container overflow y when modal is showing

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const container = document.getElementById("container");
 const trigger = document.querySelector("[data-modal]");
 const modal = document.getElementById(trigger.dataset.modal);
 const buttons = document.querySelectorAll(".modal-footer__button");
@@ -5,6 +6,7 @@ const buttons = document.querySelectorAll(".modal-footer__button");
 document.addEventListener("keydown", (event) => {
   if ((event.keyCode || event.charCode) === 27) {
     modal.classList.remove("open");
+    container.style.overflowY = "scroll";
   }
 });
 
@@ -13,12 +15,15 @@ trigger.addEventListener("click", (event) => {
 
   modal.classList.add("open");
 
+  container.style.overflowY = "hidden";
+
   const exits = modal.querySelectorAll(".modal-exit");
 
   exits.forEach((exit) => {
     exit.addEventListener("click", (event) => {
       event.preventDefault();
       modal.classList.remove("open");
+      container.style.overflowY = "scroll";
     });
   });
 
